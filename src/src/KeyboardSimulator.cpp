4e867f058736b8f82b6b5e840453544c2788a5e2
AshenOneYe <1282496341@qq.com>
2021-08-22 13:12:04 +0800

    修改后端按键映射

--- src/src/KeyboardSimulator.cpp
+++ src/src/KeyboardSimulator.cpp
@@ -19,7 +19,7 @@ WORD YUANCON_BTN_MAP[N_BUTTONS] = {
     'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q',
     'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I',
     'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A',
-    'Q', 'W', 'E','R', 'T', 'Y'};
+    'Y', 'Q', 'W','E', 'R', 'T'};
 
 struct KeyboardSimulator::Impl
 {
